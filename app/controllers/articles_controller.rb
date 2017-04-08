e740a9866ecc20ad544c961d8f49c455a220c3ae
kangethe12 <kangethemujohn@gmail.com>
2017-04-08 14:25:46 +0300

    blogs donegit add .

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
     flash.notice = "Article '#{@article.title}' view!"
   end
   def new
@@ -16,26 +18,26 @@ class ArticlesController < ApplicationController
   end
   def create
     @article = Article.new(
-      title: params[:article][:title],
-      body: params[:article][:body])
+    title: params[:article][:title],
+    body: params[:article][:body])
     @article.save
     redirect_to article_path(@article)
   end
   def destroy
-      @article=Article.find(params[:id])
-      @article.destroy
-      redirect_to article_path
+   @article = Article.find(params[:id])
+   @article.destroy
+   flash[:danger] = "Article deleted"
+   redirect_to articles_path
   end
   def edit
-  @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
   end
 
   def update
-  @article = Article.find(params[:id])
-  @article.update(article_params)
+      @article = Article.find(params[:id])
+      @article.update(article_params)
+      flash.notice = "Article '#{@article.title}' Updated!"
+      redirect_to article_path(@article)
+  end
 
-  flash.notice = "Article '#{@article.title}' Updated!"
-
-  redirect_to article_path(@article)
-end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Blogger::Application.routes.draw do
-  root to: 'articles#index'
-  resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
+    resources :articles do
+      resources :comments
+    end
+    root 'welcome#index'
+  end
